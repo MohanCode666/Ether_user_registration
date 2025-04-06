8a1284d5709d36c615cbae77d48184c01b891dd2
MohanCode666 <124455766+MohanCode666@users.noreply.github.com>
2025-04-06 19:30:35 +0530

    Update info.py

--- coin/info.py
+++ coin/info.py
@@ -1,5 +1,5 @@
 EMAIL_USE_TLS =True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ether.mail.django@gmail.com'
-EMAIL_HOST_PASSWORD = 'lnzdbyyiqybzeelj'
+EMAIL_HOST_PASSWORD = 'vbfkqxepiiuhpvwc'
 EMAIL_PORT = 587
